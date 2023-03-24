c26742b2a2c895432f6890d107a54d7cd5b032a0
Miguel Maramara <miguel.maramara@stonybrook.edu>
2023-03-24 06:23:12 -0400

    finished malloc

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -6,7 +6,11 @@ int main(int argc, char const *argv[]) {
 
     *ptr = 320320320e-320;
 
-    printf("%f\n", *ptr);
+    printf("result ptr: %p\n", ptr);
+    printf("headerLocation ptr: %p\n", (void*)((long) ptr - 8));
+    printf("%.10e\n", *ptr);
+    printf("%.10e\n", 320320320e-320);
+    sf_show_heap();
 
     sf_free(ptr);
 
--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include "debug.h"
 #include "sfmm.h"
 
@@ -45,7 +46,7 @@ sf_header * createHeader(sf_header* in, long length, long flags){
 
 sf_footer* createFooter(sf_header *headerPtr){
     long offset = maskLong(*headerPtr, 0b111, 1);
-    sf_footer * footerPtr = offsetPtr(headerPtr, offset - 8);
+    sf_footer * footerPtr = offsetPtr(headerPtr, offset - sizeof(size_t));
     *footerPtr = *headerPtr;
     return footerPtr;
 }
@@ -72,13 +73,25 @@ long getLength(sf_block *in){
     return getHeaderLen(&(in->header));
 }
 
+int isAlloc(sf_block *in){
+    return (getFlags(in) & THIS_BLOCK_ALLOCATED) == THIS_BLOCK_ALLOCATED;
+}
+
+int prevAlloc(sf_block *in){
+    return (getFlags(in) & PREV_BLOCK_ALLOCATED) == PREV_BLOCK_ALLOCATED;
+}
+
+int inQuickLst(sf_block *in){
+    return (getFlags(in) & IN_QUICK_LIST) == IN_QUICK_LIST;
+}
+
 sf_block* peerBlock(sf_block* in, int forward){
     if(forward)
         return offsetPtr(in, getLength(in));
 
     long flags = getFlags(in);
     if((flags & PREV_BLOCK_ALLOCATED) == 0){
-        sf_header *footer = offsetPtr(in, -8);
+        sf_header *footer = offsetPtr(in, -1 * sizeof(size_t));
         return offsetPtr(in, -1 * getHeaderLen(footer));
 
     } else
@@ -113,7 +126,7 @@ sf_block *updateBlock(sf_block* in, long length, long flags){
 }
 
 sf_block *updateBlockFlags(sf_block* in, int inQuickLst, int thisBlockAllocated){
-    long newFlags = maskLong(getFlags(in), 0x010, 0);
+    long newFlags = maskLong(getFlags(in), 0b010, 0);
     if(inQuickLst)
         newFlags |= IN_QUICK_LIST;
     if(thisBlockAllocated)
@@ -121,7 +134,6 @@ sf_block *updateBlockFlags(sf_block* in, int inQuickLst, int thisBlockAllocated)
     updateBlock(in, getLength(in), newFlags);
     return in;
 }
-
 int fitsInFreeList(long size, int i){
     if( i < 0) return 0;
     if(i == 0 && size == 32) return 2;
@@ -154,10 +166,101 @@ sf_block *addToFreeList(sf_block *in){
 }
 
 sf_block* removeFromFreeList(sf_block *in){
-    in->body.links.prev->body.links.next = in->body.links.next;
-    in->body.links.next->body.links.prev = in->body.links.prev;
-    in->body.links.next = 0;
-    in->body.links.prev = 0;
+    if(in->body.links.prev != NULL)
+        in->body.links.prev->body.links.next = in->body.links.next;
+    if(in->body.links.next != NULL)
+        in->body.links.next->body.links.prev = in->body.links.prev;
+    in->body.links.next = NULL;
+    in->body.links.prev = NULL;
+    return in;
+}
+
+/* 
+ * checks if forward block can be coalesced and backward block can be coalesced
+ * and coalescese them together.
+ *
+ * coalesce also handles any free lists
+ * does not add to freeList if 
+ */
+sf_block *coalesce(sf_block *in){
+    sf_block * next = peerBlock(in, 1);
+
+    // should never hit this
+    if(next >= (sf_block *)sf_mem_end())
+        return in;
+    removeFromFreeList(in);
+
+    // coalesce forward
+    while(!isAlloc(next)){
+        removeFromFreeList(next);
+        createHeaderAndFooter(&(in->header), getLength(in) + getLength(next), getFlags(in));
+        next = peerBlock(in, 1);
+    }
+
+    // coalesce backward
+    while(!prevAlloc(in)){
+        sf_block *prev = peerBlock(in, 0);
+        createHeaderAndFooter(&(prev->header), getLength(prev) + getLength(in), getFlags(prev));
+        in = prev;
+        removeFromFreeList(in);
+    }
+    addToFreeList(in);
+    return in;
+}
+
+/*
+ * returns 1 if the size fits into the quicklist, 0 if not
+ */
+int getQuickListNum(long size){
+    // if(size < 32) return 0;
+    // if(size > 32 + (NUM_QUICK_LISTS - 1) * 8) return 0;
+    long i = (size - 32) / 8;
+    if(((size - 32) & 0b111)!= 0) return -1; // must be factor of 8
+    if(i < 0 || i > NUM_QUICK_LISTS - 1) return -1;
+    if(i != (int)i) return -1; // must fit in size of int
+    return (int)i;
+}
+
+/*
+ * Just adds a block to a quick list. Does change block flags;
+ */
+sf_block *addToQuickList(sf_block *in){
+    // find correct quicklist
+    int i = getQuickListNum(getLength(in));
+
+    if( i < 0 || i >= NUM_QUICK_LISTS)
+        return NULL;
+
+    // TODO: CHECK FOR COALESCE
+    if(sf_quick_lists[i].length > QUICK_LIST_MAX){
+        sf_block *curr = sf_quick_lists[i].first;
+        while(curr != NULL){
+            updateBlock(curr, getLength(curr), 0b000);
+            addToFreeList(coalesce(curr));
+        }
+
+        sf_quick_lists[i].length = 0;
+        sf_quick_lists[i].first = NULL;
+    }
+
+    
+    // add onto the end of the quicklist
+    sf_quick_lists[i].length += 1;
+    if(sf_quick_lists[i].first == NULL){
+        sf_quick_lists[i].first = in;
+    }else{
+        sf_block* curr = sf_quick_lists[i].first;
+        while(curr->body.links.next != NULL){
+            curr = curr->body.links.next;
+        }
+        curr->body.links.next = in;
+    }
+
+    in->body.links.next = NULL;
+    updateBlockFlags(in, 1, 1);
+
+
+
     return in;
 }
 
@@ -182,6 +285,41 @@ sf_block* splitBlock(sf_block* in, long size){
     return res;
 }
 
+size_t roundUpSize(size_t size){
+    if((long)size < 0){
+        return -1;
+    }
+    
+    if(maskLong((long) size, 0b111, 0) == 0) return size;
+    return size + maskLong((long)~size, 0b111, 0) + 1;
+}
+
+/*
+ * Makes heap larger, expands epilogue and coalesces the former epilogue
+ */
+sf_block *expandHeap(){
+    sf_block *oldEp = offsetPtr(sf_mem_end(), -1 * sizeof(size_t));
+
+    // make heap larger
+    if(sf_mem_grow() == NULL)
+        return NULL;
+    sf_block *newEp = offsetPtr(sf_mem_end(), -1 * sizeof(size_t));
+
+    //expand epilogue
+    createHeaderAndFooter(
+            &(oldEp->header),
+            (long)newEp - (long)oldEp,
+            maskLong(getFlags(oldEp), 0b010 , 0) | 0b000);
+
+    // put headers on new epilogue
+    updateBlock(newEp, sizeof(size_t), 0b001);
+
+    sf_block *res = coalesce(oldEp);
+    // coalesce epilogue
+    return res;
+    // return coalesce(oldEp);
+}
+
 
 void* initHeap(){
     // request more memory
@@ -190,14 +328,20 @@ void* initHeap(){
     
     // create initial pointers
     sf_block* prologue = sf_mem_start();
-    sf_block* epilogue = offsetPtr(sf_mem_end(), -8);
+    sf_block* epilogue = offsetPtr(sf_mem_end(), -1 * sizeof(size_t));
     sf_block* freeBlock = offsetPtr(prologue, 32);
 
 
     // create blocks for data
     updateBlock(prologue, 32, 0b001);
-    updateBlock(epilogue, 8, 0b001);
+    updateBlock(epilogue, sizeof(size_t), 0b001);
     updateBlock(freeBlock, (long)epilogue- (long)freeBlock, 0b010);
+
+    // initialize quicklist
+    for(int i = 0; i < NUM_QUICK_LISTS; i++){
+        sf_quick_lists[i].length = 0;
+        sf_quick_lists[i].first = NULL;
+    }
     
     // initialize freelist
     for(int i = 0; i < NUM_FREE_LISTS; i++){
@@ -212,22 +356,85 @@ void* initHeap(){
 }
 
 void *sf_malloc(size_t size) {
+    if(size == 0)
+        return NULL;
+
     // initialize heap
     if(sf_mem_start() == sf_mem_end()){
         sf_block* freeBlock =
-            initHeap();
+        initHeap();
         
-        sf_show_heap();
-        sf_block* res = splitBlock(freeBlock, 4056/2 - ((4056/2)%8));
-        
-        sf_show_heap();
-        updateBlockFlags(res, 0, 1);
-        // create prologue
+        // sf_show_heap();
+        // sf_block* q2 = splitBlock(freeBlock, 4056/2 - ((4056/2)%8));
+        // sf_show_heap();
+
+        /*sf_block* q1 = */splitBlock(freeBlock, 56);
+        // splitBlock(freeBlock, 32);
+        // splitBlock(q2, 184);
+
+        // sf_show_heap();
+        removeFromFreeList(freeBlock);
+        addToQuickList(freeBlock);
+        // sf_show_heap();
+        // addToQuickList(q1);
+        // addToQuickList(q2);
     }
-    sf_show_heap();
+
+    size_t test = 85912;
+    size = roundUpSize(test); // switch to size later on
+    size_t blockSize =  // round up to minimum block size
+        size + sizeof(size_t) < sizeof(size_t) * 2 + sizeof(sf_block*) * 2
+        ? sizeof(size_t) * 2 + sizeof(sf_block*) * 2
+        : size + sizeof(size_t);
+
+    // Search quick lists
+    int i = getQuickListNum(blockSize);
+    if(i >= 0 && sf_quick_lists[i].length > 0){
+        sf_block *res = sf_quick_lists[i].first;
+        sf_quick_lists[i].first = res->body.links.next;
+
+        updateBlockFlags(res, 0, 1);
+        return offsetPtr(res, sizeof(size_t));
+    }
+    
+    // Search free lists
+    // search through all free lists
+    for(int i = 0; i < NUM_FREE_LISTS; i++){
+        if(fitsInFreeList(blockSize, i)){
+            sf_block *curr = sf_free_list_heads[i].body.links.next;
+            // search though every block in free list
+            while(curr != &sf_free_list_heads[i]){
+                if(getLength(curr) >= blockSize){
+                    splitBlock(curr, blockSize); //attempt to trim block
+
+                    removeFromFreeList(curr);
+                    updateBlockFlags(curr, 0, 1);
+                    return offsetPtr(curr, sizeof(size_t));
+                }
+                curr = curr->body.links.next;
+            }
+        }
+    }
+
+    
+    // expandHeap heap until you find necesary size, or you can't anymore
+
+    while(true){
+        sf_block* newMem = expandHeap();
+        if(newMem == NULL)
+            break;
+        if(getLength(newMem) >= blockSize){
+            removeFromFreeList(newMem);
+            updateBlockFlags(newMem, 0, 1);
+            return offsetPtr(newMem, sizeof(size_t));
+       }
+    }
     
     // TO BE IMPLEMENTED
-    abort();
+    // abort();
+    sf_errno = ENOMEM;
+    fprintf(stderr, "TOOOOO BIG\n");
+    return NULL;
 }
 
 void sf_free(void *pp) {
